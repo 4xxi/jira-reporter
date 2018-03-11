dab18c72b4666e7b678e2837e1b3060888ace448
Evgeny Smirnov <smirik@gmail.com>
2018-03-11 15:03:50 +0300

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,24 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     // the project directory where compiled assets will be stored
+    // .enableSassLoader(function(sassOptions) {}, {
+    //     resolveUrlLoader: false
+    // })
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
+    // uncomment to define the assets of the project
+    .addEntry('app', './assets/js/app.js')
+    // .addStyleEntry('app', './assets/css/app.scss')
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
+    // uncomment for legacy applications that require $/jQuery as a global variable
+    .autoProvidejQuery()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
 
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
